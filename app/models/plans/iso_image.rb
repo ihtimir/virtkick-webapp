7dbfd12471d4dfda2ab212c1c6c015e007ed4a91
Nowaker <damian.nowak@geozone.pl>
2014-09-14 04:53:01 +0200

    machine create screen - provide full path to the ISO images

--- app/models/plans/iso_image.rb
+++ app/models/plans/iso_image.rb
@@ -3,11 +3,17 @@ class Plans::IsoImage < ActiveYaml::Base
   set_root_path 'app/models'
 
   field :id
-  field :path
+  field :file
+  field :pool_name
   field :bit
   field :short_name
   field :full_name
   field :enabled, default: true
 
   belongs_to :iso_distro, class_name: 'Plans::IsoDistro'
+
+
+  def path
+    DiskType.find(pool_name).path + '/' + file
+  end
 end
