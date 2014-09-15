807313c35b6d99fb9516a467f6669286485afe17
Nowaker <damian.nowak@geozone.pl>
2014-09-15 06:38:01 +0200

    make machine power actions POST only

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   resources :machines do
     member do
-      get 'start' # TODO: needs to be POST for security reasons
-      get 'pause'
-      get 'resume'
-      get 'stop'
-      get 'force_stop'
-      get 'restart'
-      get 'force_restart'
+      post 'start'
+      post 'pause'
+      post 'resume'
+      post 'stop'
+      post 'force_stop'
+      post 'restart'
+      post 'force_restart'
 
       post 'mount_iso'
     end
