956ff5c220bd3e8cd64a00447320d7e491ef3a0e
Nowaker <damian.nowak@geozone.pl>
2014-09-24 15:47:53 +0200

    reduce biggest plans so as our 128GB hypervisor doesn't blow up during alpha tests

--- app/models/defaults/machine_plan.rb
+++ app/models/defaults/machine_plan.rb
@@ -5,8 +5,8 @@ class Defaults::MachinePlan < ActiveHash::Base
       [0.5, 20, 'HDD', 1],
       [1, 30, 'HDD', 1],
       [2, 50, 'HDD', 2],
-      [4, 70, 'HDD', 2],
-      [8, 90, 'HDD', 3]
+      # [4, 70, 'HDD', 2],
+      # [8, 90, 'HDD', 3]
   ]
 
   self.data = plans.map.with_index do |plan, i|
